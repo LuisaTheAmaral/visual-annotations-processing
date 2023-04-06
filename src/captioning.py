e6a86f8bc5d8d63d7ef853d2a0f8bb457ee9b4e8
Luísa <luisa.amaral9@gmail.com>
2023-04-07 00:01:52 +0100

    add filtering to grit detections and depth to overlap check

--- src/captioning.py
+++ src/captioning.py
@@ -36,10 +36,10 @@ def get_max_similarity(str1, str2):
 
 #returns ratio of overlap of two bounding boxes
 def get_overlap_ratio(bb1, bb2):
-    xmin, ymin, xmax, ymax = bb1
-    xmin2, ymin2, xmax2, ymax2 = bb2
+    xmin, ymin, xmax, ymax, _, plane1 = bb1
+    xmin2, ymin2, xmax2, ymax2, _, plane2 = bb2
     overlap = []
-    if not (xmin > xmax2 or xmin2 > xmax) or not (ymax > ymin2 or ymax2 > ymin):
+    if not (xmin > xmax2 or xmin2 > xmax) or not (ymax > ymin2 or ymax2 > ymin) and plane1 == plane2:
         # boxes overlap
         # area of intersection
         SI = max(0, min(xmax, xmax2) - max(xmin, xmin2)) * max(0, min(ymax, ymax2) - max(ymin, ymin2))
@@ -58,12 +58,32 @@ def merge_object_detections(annotations):
     grit = annotations['grit']
     yolo = annotations['yolo']
 
+    # Compute the overlap ratios and similarity ratios between elements in grit
+    for i, obj1 in enumerate(grit):
+        keep_obj1 = True
+        keep_obj2 = True
+        for j, obj2 in enumerate(grit[i+1:]):
+            j = i + j + 1  # adjust index for nested loop
+            keep_obj2 = True
+            overlap_ratio = get_overlap_ratio(obj1[1:], obj2[1:])
+            similarity = get_max_similarity(obj1[0], obj2[0])
+            if overlap_ratio >= 0.7 or (0.4 <= overlap_ratio < 0.7 and similarity > 0.5):
+                if len(obj1[0]) >= len(obj2[0]):
+                    if obj2 in grit:
+                        grit.remove(obj2)
+                        #print(f"Object {obj2[0]} was excluded by {obj1[0]}")
+                else:
+                    if obj1 in grit:
+                        grit.remove(obj1)
+                        #print(f"Object {obj1[0]} was excluded by {obj2[0]}")
+                        break #object 1 is out so no need to compare it with the remaining objects
+
     # Compute the overlap ratios and similarity ratios between elements in yolo and grit
     overlap_ratios = np.zeros((len(yolo), len(grit)))
     similarity_ratios = np.zeros((len(yolo), len(grit)))
     for i, elem1 in enumerate(yolo):
         for j, elem2 in enumerate(grit):
-            overlap_ratios[i, j] = get_overlap_ratio(elem1[1:-1], elem2[1:-1])
+            overlap_ratios[i, j] = get_overlap_ratio(elem1[1:], elem2[1:])
             similarity_ratios[i, j] = get_max_similarity(elem1[0], elem2[0])
 
     # Filter out elements from yolo that have an overlap ratio greater than or equal to 0.7 with any element in grit
@@ -76,11 +96,11 @@ def merge_object_detections(annotations):
                 merged_list.append(elem1)
                 break
 
-    # Combine the filtered yolo and grit to create the final merged list
+    # Combine the filtered yolo and grit lists to create the final merged list
     return merged_list + grit
 
 def map_objects_to_planes(annotations, depth_map):
-    for obj in annotations:
+    for obj in annotations['yolo'] + annotations['grit'] + annotations['craft']:
         x0, y0, x1, y1 = obj[1:-1]
         region = depth_map[math.floor(y0):math.ceil(y1), math.floor(x0):math.ceil(x1)]
         plane = stats.mode(region, axis=None)[0]
@@ -96,16 +116,20 @@ if __name__ == "__main__":
     annotations = json.load(f)
     f.close()
     
-    # filter object detections to eliminate redundant detections
-    filtered_object_detections = merge_object_detections(annotations)
-
     # create depth map of the image and binarize it
     depth_map = get_depth_map('../imgs/20190831_070629_000.jpg')
     bin_depth_map = binarize_depth_map(depth_map)
-    
-    # use depth map to establish which objects are on the foreground and which are on the background
-    map_objects_to_planes(filtered_object_detections, bin_depth_map)
 
+    # use depth map to establish which objects are on the foreground and which are on the background
+    map_objects_to_planes(annotations, bin_depth_map)
+    
+    # filter object detections to eliminate redundant detections
+    filtered_object_detections = merge_object_detections(annotations)
+    print(filtered_object_detections)
+    
+    
+    # print(get_overlap_ratio([1.518, 166.434, 234.521, 766.3079, 0, 'foreground'], [1.9926, 235.8012, 214.8844, 535.3787,0, 'foreground']))
+    # print(get_max_similarity('man wearing a black jacket and black pants', 'man wearing a black jacket'))
 
 
 
